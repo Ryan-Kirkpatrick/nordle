dfe3fca69147d09c1ecefc54fed428aabf39969d
Ryan Kirkpatrick <ryan.kptk@outlook.com>
2026-05-21 23:01:30 +1000

    Formatted. Added format check.

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,23 +2,28 @@ import pytest
 from app import create_app
 from app.extensions import db
 
+
 @pytest.fixture
 def app():
     app = create_app()
 
-    app.config.update({
-        "TESTING": True,
-        "SQLALCHEMY_DATABASE_URI": "sqlite:///:memory:",
-    })
+    app.config.update(
+        {
+            "TESTING": True,
+            "SQLALCHEMY_DATABASE_URI": "sqlite:///:memory:",
+        }
+    )
 
     with app.app_context():
         db.create_all()
         yield app
         db.drop_all()
 
+
 @pytest.fixture
 def client(app):
     return app.test_client()
+
 
 @pytest.fixture
 def runner(app):
